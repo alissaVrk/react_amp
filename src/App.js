d3011cc7397841a3e03a690e26e5b63e2cc0f2b7
alissaVrk <alissa.vrk@gmail.com>
2020-09-24 16:18:50 +0300

    bring back auth

--- src/App.js
+++ src/App.js
@@ -79,5 +79,5 @@ const styles = {
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
 
-// export default withAuthenticator(App)
-export default App;
+export default withAuthenticator(App)
+// export default App;
